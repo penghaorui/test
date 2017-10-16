b33ebf392ed78eac935cf4b4583e87434e9274eb
penghaorui <32850029+penghaorui@users.noreply.github.com>
2017-10-16 21:26:56 +0200

    Create test_code.f90

---
+++ test_code.f90
@@ -0,0 +1,17 @@
+PROGRAM test_code
+  use specfem_par
+  use shared_parameters
+  IMPLICIT NONE
+
+
+  !use read_value_parameters
+
+
+
+  print*, "hi china"
+
+  ! initialize
+  call read_parameter_file_init()
+
+END PROGRAM test_code
+
